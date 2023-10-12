96a462a4ec691aeae279fd1980708e447ceb771c
pamelamaso <pamelamaso@hotmail.com>
2023-10-12 20:13:18 -0300

    validando os metodos

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -6,5 +6,27 @@ public class Usuario {
         System.out.println("TV ligada ?: " + smartTv.ligada);
         System.out.println("Canal ?: " + smartTv.canal);
         System.out.println("Volume ?: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status da TV: "+smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status da TV: "+ smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Novo Status do Volume: "+smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Novo Status do Volume: " + smartTv.volume);
+
+        smartTv.trocarCanal(13);
+        System.out.println("Status do Canal: "+smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Diminuir um Canal: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Aumentar um Canal: " + smartTv.canal);
+
     }
 }
